fb735cf41ab1bd41ee586fef77197ad71bfdbd37
John Tuttle <jtuttle@craw.cc>
2014-12-09 08:45:13 -0500

    fix a bug that snuck in

--- Voxels/Assets/Code/Model/World.cs
+++ Voxels/Assets/Code/Model/World.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 public class World : MonoBehaviour {
     public float[,] Samples { get; private set; }
-    public Dictionary<XY, WorldScreenComponent> Screens { get; private set; }
+    public Dictionary<XY, WorldScreen> Screens { get; private set; }
 
     public World(float[,] samples) {
         Samples = samples;
