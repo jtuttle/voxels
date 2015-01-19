c1952a856227f1b9add6ceae5037d5690583c559
John Tuttle <jtuttle@craw.cc>
2015-01-19 11:34:43 -0500

    rename room, add room edge

---
+++ Voxels/Assets/Code/Model/RoomEdge.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomEdge {
+    public Room Room { get; private set; }
+    //public Symbol Symbol { get; private set; }
+
+    public RoomEdge(Room room) {
+        Room = room;
+    }
+}
