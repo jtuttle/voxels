7cd7d58f52d0fe79685422177a41cf526a7b4b60
John Tuttle <jtuttle@craw.cc>
2015-03-17 08:17:01 -0400

    basic quest system files

---
+++ Voxels/Assets/Code/Utils/Quest/QuestManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager {
+
+    private List<BaseQuestAction> _enabledQuests;
+    private List<BaseQuestAction> _completedQuests;
+}
